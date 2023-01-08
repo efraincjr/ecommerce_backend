1c9a262b15d946637aff91fa37c9339aea6d669f
Efrain Castro Jr <efraincastrojrGmail.com>
2023-01-08 15:12:56 -0600

    severjs and connection js update

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS ecommercebackend_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE ecommercebackend_db;
 
