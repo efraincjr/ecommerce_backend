0f3bc2327fe3ebc698bd40d1dd0149f672c7e120
Efrain Castro Jr <efraincastrojrGmail.com>
2023-01-12 13:09:13 -0600

    category routes uodate

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,18 +14,60 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, { include: [{ model: Product }] }).then(
+    (categoryData) => {
+      res.json(categoryData);
+    }
+  );
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      id: req.body.id,
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((destroyedCategory) => {
+      res.json(destroyedCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
